e18a2aab969948399c6a4b5316c37023539eceea
Alex <therebeloper@gmail.com>
2019-02-27 18:28:47 +0200

    Initial Commit

---
+++ PlaceHolderTextView/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  PlaceHolderTextView
+//
+//  Created by Alex Nagy on 27/02/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
